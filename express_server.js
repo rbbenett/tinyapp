9c237cbc9aa948ed970de394582db4a780b11527
Robert <rbbenett0614@gmail.com>
2020-12-07 21:15:08 +0000

    Add a GET Route to Show the FOrm

--- express_server.js
+++ express_server.js
@@ -4,6 +4,9 @@ const PORT = 8080; // default port 8080;
 
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 const urlDatatbase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://google.com"
@@ -12,6 +15,10 @@ const urlDatatbase = {
 app.get("/urls", (req, res) => {
   const templateVars = {urls: urlDatatbase};
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
