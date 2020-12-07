996dc4956c2025cc247cafc6f1311779c064aa0f
Robert <rbbenett0614@gmail.com>
2020-12-07 21:21:50 +0000

    Add a POST Route to Receive the Form Submission

--- express_server.js
+++ express_server.js
@@ -2,10 +2,19 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080;
 
+function generateRandomString() {
+
+};
+
 app.set("view engine", "ejs");
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 const urlDatatbase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
