f281ac37cbb9c8efa6ce1bd573abc6a748667312
Robert <rbbenett0614@gmail.com>
2020-12-08 01:26:00 +0000

    Added redirect

--- express_server.js
+++ express_server.js
@@ -1,52 +1,65 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080;
-
-function generateRandomString() {
-
-};
-
-app.set("view engine", "ejs");
-
+const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
-});
+app.set("view engine", "ejs");
 
-const urlDatatbase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://google.com"
+function generateRandomString() {
+  let randomString = '';
+  const characterList = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let x = 0; x < 6; x += 1) {
+    randomString += characterList.charAt(Math.floor(Math.random() * characterList.length));
+  }
+  return randomString;
 };
 
-app.get("/urls", (req, res) => {
-  const templateVars = {urls: urlDatatbase};
-  res.render("urls_index", templateVars);
-});
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.post('/urls', (req, res) => {
+  const newShortURL = generateRandomString();
+  const newLongURL = req.body.longURL;
+  urlDatabase[newShortURL] = `http://${newLongURL}`;
+  res.redirect('http://localhost:8080/urls/');
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  if (urlDatabase[req.params.shortURL]) {
+    fullURL = urlDatabase[req.params.shortURL];
+    res.redirect(fullURL);
+  }
 });
 
 app.get("/urls.json", (req, res) => {
-  res.json(urlDatatbase);
+  res.json(urlDatabase);
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
 app.listen(PORT, () => {
-  console.log(`Example app listening on ${PORT}!`);
+  console.log(`Example app listening on port ${PORT}!`);
 });
