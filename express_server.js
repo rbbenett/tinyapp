48f064560fb9c6b7d412cc3147058a4006bac529
Robert <rbbenett0614@gmail.com>
2020-12-09 23:44:06 +0000

    Added Login and Registration Page

--- express_server.js
+++ express_server.js
@@ -3,11 +3,24 @@ const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
-
 app.set("view engine", "ejs");
+
+//                      STORAGE
+// =======================================================
+
+// Stores database of URLs
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+// Stores database of user information
+const users = {};
+
+//                      FUNCTIONS
+// =======================================================
 
 // Genarates a random string of 6 letter
 const generateRandomString = function() {
@@ -19,27 +32,7 @@ const generateRandomString = function() {
   return randomString;
 };
 
-// Stores database of URLs
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
-
-// Stores database of user information
-const users = {
-  "userRandomID": {
-    id: "userRandomID",
-    email: "user@example.com",
-    password: "purple-monkey-dinosaur"
-  },
-  "user2RandomID": {
-    id: "user2RandomID",
-    email: "user2@example.com",
-    password: "dishwasher-funk"
-  }
-};
-
-// Add new user to database
+// Adds new user to database
 const addNewUser = function(email, password, database) {
   let userID = generateRandomString();
   const newUser = {
@@ -51,6 +44,7 @@ const addNewUser = function(email, password, database) {
   return userID;
 };
 
+// Checks to see if email is already in database
 const checkEmailExists = function(userEmail) {
   for (const id in users) {
     if (users[id]['email'] === userEmail) {
@@ -60,26 +54,50 @@ const checkEmailExists = function(userEmail) {
   return false;
 };
 
+// Checks to see if password is already in database
+const checkPasswordExists = function(password) {
+  for (const id in users) {
+    if (users[id]["password"] === password) {
+      return true;
+    }
+  }
+  return false;
+};
+
+// Returns the ID associated with user's email
+const getIDByEmail = function(userEmail) {
+  for (const id in users) {
+    if (users[id]['email'] === userEmail) {
+      return users[id]["id"];
+    }
+  }
+};
+
+//                          GET REQUESTS
+// =========================================================
+
 // Creates new page
 app.get("/urls/new", (req, res) => {
-  const templateVars = {user: users[req.cookies['userID']]};
+  const templateVars = {user: users[req.cookies['user_id']]};
   res.render("urls_new", templateVars);
 });
 
 // Creates the page for each URL
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: users[req.cookies['userID']]};
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: users[req.cookies['user_id']]};
   res.render("urls_show", templateVars);
 });
 
 // Creates login page
 app.get('/login', (req, res) => {
-  res.render('login');
+  const templateVars = {user: users[req.cookies['user_id']]};
+  res.render('login', templateVars);
 });
 
 // Creates registration page
 app.get('/register', (req, res) => {
-  res.render('register');
+  const templateVars = {user: users[req.cookies['user_id']]};
+  res.render('register', templateVars);
 });
 
 // Adds new shortened URL page to database
@@ -96,15 +114,12 @@ app.get("/urls.json", (req, res) => {
 
 // Creates main page showing all the URLs
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase, user: users[req.cookies['userID']] };
+  const templateVars = { urls: urlDatabase, user: users[req.cookies['user_id']] };
   res.render("urls_index", templateVars);
 });
 
-// Saves the login info to the website
-app.post('/login', (req, res) => {
-  res.cookie('username', req.body.username);
-  res.redirect('/urls');
-});
+//                        POST REQUESTS
+// =========================================================
 
 // Saves new user registration to database
 app.post('/register', (req, res) => {
@@ -116,9 +131,30 @@ app.post('/register', (req, res) => {
     res.status(400).send("Error: This email already exists!");
   } else {
     const userID = addNewUser(email, password, users);
-    res.cookie('userID', userID);
+    res.cookie('user_id', userID);
     res.redirect('/urls');
   }
+});
+
+// Saves the login info to the website
+app.post('/login', (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
+  if (checkEmailExists(email) === false) {
+    res.status(403).send("Error: Couldn't find an account with that email!");
+  } else if (checkEmailExists(email) === true && checkPasswordExists(password) === false) {
+    res.status(403).send("Error: Incorrect Password!");
+  } else {
+    const userID = getIDByEmail(email);
+    res.cookie('user_id', userID);
+    res.redirect('/urls');
+  }
+});
+
+// Allows user to logout and clear the cookie from the website
+app.post("/logout", (req, res) => {
+  res.clearCookie('user_id', req.body.user_ID);
+  res.redirect('/urls');
 });
 
 // Saves new URLs to main page database
@@ -140,12 +176,6 @@ app.post('/urls/:url_id', (req, res) => {
   const key = req.params.url_id;
   const longURL = req.body.longURL;
   urlDatabase[key] = longURL;
-  res.redirect('/urls');
-});
-
-// Allows user to logout and clear the cookie from the website
-app.post("/logout", (req, res) => {
-  res.clearCookie('username', req.body.username);
   res.redirect('/urls');
 });
 
